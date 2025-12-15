850791ed0384034b833eab64edfbd5d3a84d6bbe
MalekSum <malekhammache51@gmail.com>
2025-12-15 18:39:22 +0100

    Update evaluate.py

--- src/evaluate.py
+++ src/evaluate.py
@@ -1,27 +1,23 @@
-# src/evaluate.py
-
 import matplotlib.pyplot as plt
 import seaborn as sns
 from sklearn.metrics import accuracy_score, confusion_matrix, classification_report
 
 
-def evaluate_model(model, X_test, y_test, title="Model"):
+def evaluate_model(model, X_test, y_test, title):
 
     y_pred = model.predict(X_test)
 
     acc = accuracy_score(y_test, y_pred)
     cm = confusion_matrix(y_test, y_pred)
 
-    print(f"\n==== {title} ====\n")
-    print(f"Accuracy: {acc:.4f}")
-    print("\nConfusion Matrix:\n", cm)
-    print("\nClassification Report:\n", classification_report(y_test, y_pred))
+    print("\n", title)
+    print("Accuracy:", acc)
+    print("Confusion Matrix:\n", cm)
+    print("Classification Report:\n", classification_report(y_test, y_pred))
 
-    plt.figure(figsize=(5,4))
+    plt.figure()
     sns.heatmap(cm, annot=True, fmt="d", cmap="Blues")
-    plt.title(f"{title} - Confusion Matrix")
+    plt.title(title)
     plt.xlabel("Predicted")
     plt.ylabel("True")
     plt.show()
-
-    return acc, y_pred
